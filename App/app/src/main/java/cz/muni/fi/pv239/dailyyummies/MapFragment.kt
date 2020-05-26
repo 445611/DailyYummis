4a89a854189fc23031a238f7cd7e31728179e2bf
xjancus <p.jancus1996@gmail.com>
2020-05-26 12:12:03 +0200

    Save the unfinished users location functionality

--- App/app/src/main/java/cz/muni/fi/pv239/dailyyummies/MapFragment.kt
+++ App/app/src/main/java/cz/muni/fi/pv239/dailyyummies/MapFragment.kt
@@ -1,14 +1,20 @@
 package cz.muni.fi.pv239.dailyyummies
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import cz.muni.fi.pv239.dailyyummies.model.SharedViewModel
 import kotlinx.android.synthetic.main.fragment_map.*
 
@@ -17,8 +23,13 @@ import kotlinx.android.synthetic.main.fragment_map.*
  */
 class MapFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var googleMap: GoogleMap
+    private lateinit var mMap: GoogleMap
     private lateinit var mapFragment: SupportMapFragment
+    private lateinit var lastLocation: Location
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     private val viewModel: SharedViewModel by activityViewModels()
 
@@ -39,10 +50,13 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         return view
     }
 
-    override fun onMapReady(map: GoogleMap?) {
-        map?.let {
-            googleMap = it
-        }
+    override fun onMapReady(map: GoogleMap) {
+        mMap = map
+
+        val home = LatLng(49.2057192, 16.5991987)
+        mMap.addMarker(MarkerOptions().position(home).title("Marker almost at home"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 12.0f))
+
     }
 
 
