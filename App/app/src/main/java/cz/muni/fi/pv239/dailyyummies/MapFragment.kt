2bc417f8dc6f964ed0fda032b52bb8a396a4cec1
xjancus <p.jancus1996@gmail.com>
2020-05-26 11:09:28 +0200

    Map created, no functionality yet.

--- App/app/src/main/java/cz/muni/fi/pv239/dailyyummies/MapFragment.kt
+++ App/app/src/main/java/cz/muni/fi/pv239/dailyyummies/MapFragment.kt
@@ -6,12 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import cz.muni.fi.pv239.dailyyummies.model.SharedViewModel
+import kotlinx.android.synthetic.main.fragment_map.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
+    private lateinit var mapFragment: SupportMapFragment
 
     private val viewModel: SharedViewModel by activityViewModels()
 
@@ -19,13 +26,24 @@ class MapFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_map, container, false)
+        mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         return view
     }
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let {
+            googleMap = it
+        }
+    }
+
+
 }
